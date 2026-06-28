12d5685eab314537b7f6df3a2f53027273b7b8c6
Kennedy Santos da Silva <75652821+kennedyvoid@users.noreply.github.com>
2026-06-28 03:16:13 -0300

    Delete BJJAcademyFlutter directory

--- BJJAcademyFlutter/android/app/src/main/kotlin/com/example/bjj_academy/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bjj_academy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- BJJAcademyFlutter/lib/models/prohibited_technique.dart
+++
@@ -1,9 +0,0 @@
-import 'belt.dart';
-
-class ProhibitedTechnique {
-  final Belt belt;
-  final String name;
-  final String reason;
-
-  const ProhibitedTechnique({required this.belt, required this.name, required this.reason});
-}
